e302daa4d6b199c1633f5141f33a30a397b994ab
PhamThiKimNgan <Babe270103@gmail.com>
2025-02-24 11:11:04 +0700

    add payment, convert project to ts

--- FE/src/models/index.ts
+++
@@ -1,6 +0,0 @@
-export type * from "./oder-item.model";
-export type * from "./oder.model";
-export type * from "./product.model";
-export type * from "./shipping-address.model";
-export type * from "./user.model";
-export type * from "./warranty.model";
